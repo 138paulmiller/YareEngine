8f4d449c2c75a1202819b206d4391c71b367b478
138paulmiller <138paulmiller@gmail.com>
2019-09-08 11:01:42 -0700

    Render api design outline

--- YareEngine/Yare/Graphics/Renderer.hpp
+++ YareEngine/Yare/Graphics/Renderer.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include "Graphics.hpp"
 #include "Camera.hpp"
+#include "Shader.hpp"
 YARE_GRAPHICS_MODULE_BEG
 
 class Renderable
@@ -12,11 +13,42 @@ class Renderable
 	unsigned int _shaderId;
 };
 
+//Test functions to stencil, depth, test etc....
+enum class RenderTestFunc
+{
+	Never =0 , //Always fails.
+	Always, //Always passes
+	Less,
+	LessEqual,
+	Greater,
+	GreaterEqual,
+	Equal, 
+	NotEqual
+};
+enum class RenderWinding {
+	None =0 , Clockwise, CounterClockwise
+};
+
+enum class RenderCullFace {
+	None = 0, FrontFace, BackFace
+};
+
+
+struct RenderConfig
+{
+	RenderCullFace cullFace;
+	RenderWinding  winding;
+	RenderTestFunc depthFunc;
+	RenderTestFunc stencilFunc;
+};
 
 struct RenderCommand
 {
-	//camera, shader, scene[octree of renderables], framebuffer, options (faceculling etc...) 
-
+	//RenderTarget target; //render target can have multiple attachments
+	//Scene * scene;
+	Camera * camera;
+	Shader * shader;
+	RenderConfig config;
 };
 
 //Runs on its own thread 
@@ -26,9 +58,10 @@ class Renderer
 public:
 	virtual ~Renderer() = default;
 	virtual void render(const RenderCommand & command) = 0;
-
-
-	//add cull face, depoth test vars etc...
+	/*
+		cached config state. render command only updates if different
+	*/
+	RenderConfig config;
 };
 
 YARE_GRAPHICS_MODULE_END
