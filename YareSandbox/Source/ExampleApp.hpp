6d2072ca3cc97fb42bf46d45f2a4781c61e50855
138paulmiller <138paulmiller@gmail.com>
2020-01-24 12:46:45 -0800

    2 lights with 100 objects run 30fps on 30Hz display

--- YareSandbox/Source/ExampleApp.hpp
+++ YareSandbox/Source/ExampleApp.hpp
@@ -43,8 +43,8 @@ class ExampleApp : public App
 	float _elapsedTime = 0.f;
 	int _frames = 0;
 	
-	const int BOX_COUNT = 50;
-	const int LIGHT_COUNT = 4;
+	const int BOX_COUNT = 100;
+	const int LIGHT_COUNT = 2;
 
 	Scene _scene;
 	std::unique_ptr<PerspectiveCamera >_camera;
